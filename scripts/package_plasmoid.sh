43e8c30f5af3c82029b556cf9457608213a9ce8e
x211321 <49248821+x211321@users.noreply.github.com>
2022-12-31 20:35:35 +0100

    Update package_plasmoid.sh

--- scripts/package_plasmoid.sh
+++ scripts/package_plasmoid.sh
@@ -6,7 +6,7 @@ cd ..
 version=$(awk -F "=" '/X-KDE-PluginInfo-Version/ {print $2}' ./package/metadata.desktop)
 
 rm ./scripts/RGB-Config-Acer_$version.plasmoid
-zip -r ./scripts/RGB-Config-Acer_$version.plasmoid ./package
+zip -r ./scripts/RGB-Config-Acer_$version.plasmoid ./package -x ./package/translate/\*
 
 cd "${0%/*}"
 
